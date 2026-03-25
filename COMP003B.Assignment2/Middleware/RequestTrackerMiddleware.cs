8e5a596b7974fd0765606253843414154d0a6fd0
samian <sam.iannazzo@yahoo.com>
2026-03-24 19:55:41 -0700

    added structure to middleware class inside middleware folder

---
+++ COMP003B.Assignment2/Middleware/RequestTrackerMiddleware.cs
@@ -0,0 +1,19 @@
+﻿namespace COMP003B.Assignment2.Middleware
+{
+    public class RequestTrackerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestTrackerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            Console.WriteLine($"[Request] {context.Request.Method} {context.Request.Path}");
+            await _next(context);
+            Console.WriteLine($"[Response] {context.Response.StatusCode}");
+        }
+    }
+}
