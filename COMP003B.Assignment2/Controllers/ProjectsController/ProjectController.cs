7a48b5b67a13a871bfbdbef6d876ec5a9d74b4b4
samian <sam.iannazzo@yahoo.com>
2026-03-24 20:09:35 -0700

    added file ProjectController / controller mvc inside with IActionResult for Current, Completed, Ideas

---
+++ COMP003B.Assignment2/Controllers/ProjectsController/ProjectController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment2.Controllers.ProjectsController
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Current()
+        {
+            return View();
+        }
+
+        [HttpGet]
+
+        public IActionResult Completed()
+        {
+            return View();
+        }
+
+        [HttpGet] 
+
+        public IActionResult Ideas()
+        {
+            return View();
+        }
+    }
+}
